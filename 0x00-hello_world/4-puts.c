f66ee8d8f57a2eaf2b1f61f9c8546b80b637e7d9
ABOELDAHAB <karemali012345@gmail.com>
2023-08-10 15:35:34 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function 
+ *
+ * Return: Alawys 0 (Success)
+ **/
+
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
