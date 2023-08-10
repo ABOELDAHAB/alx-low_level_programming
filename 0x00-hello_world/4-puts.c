6cc3a6f75121802a1cfd681f43da8f36945501ff
ABOELDAHAB <karemali012345@gmail.com>
2023-08-10 16:04:32 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function 
+ * Description: A C program that prints with put function
  *
  * Return: Alawys 0 (Success)
- **/
+*/
 
-
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
