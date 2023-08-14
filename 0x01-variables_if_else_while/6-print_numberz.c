02303f8d2986fbdf830ab250c31e8b16dfab87dd
ABOELDAHAB <karemali012345@gmail.com>
2023-08-14 07:56:20 -0700

    second task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers from 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + 48);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
