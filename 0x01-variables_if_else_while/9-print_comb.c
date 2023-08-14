ede7d2e75de1db435b8c614a911d3708874276da
ABOELDAHAB <karemali012345@gmail.com>
2023-08-14 08:57:13 -0700

    second task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers from 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + 48);
+		if (n != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
