da6ba4353d4c895413e7fe6a88df796f67334be5
ABOELDAHAB <karemali012345@gmail.com>
2023-08-19 04:41:47 -0700

    second task

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+task for functions and nested loops
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <main.h>
+/**
+ * _putchar - writes the characters
+ * @c: the character to print
+ *
+ * Return: on success 1
+*/
+int _putchar(char c)
+{
+	return (write(1, &v, 1));
+}
