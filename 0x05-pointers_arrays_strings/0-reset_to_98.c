41f99d1fe487af2274b081209bd73200c304109c
ABOELDAHAB <karemali012345@gmail.com>
2023-08-21 17:23:06 -0700

    second task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of pointer int parameter
+ *to 98
+ * @n: int pointer parameter
+ *
+ * Return: Always 0 (success)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
