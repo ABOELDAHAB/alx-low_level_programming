5237fed5081543e716acf4e10b89f69881387318
ABOELDAHAB <karemali012345@gmail.com>
2023-08-22 14:59:13 -0700

    second task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - function that print half string
+ * @str: parametr that store string
+ *
+ * Return: Always 1 (success)
+*/
+
+void puts_half(char *str)
+{
+	int l;
+
+	for (l = 0; str[l] != '\0'; l++)
+
+	i++;
+	for (l /= 2; str[l] != '\0'; l++)
+	{
+		_putchar(str[l]);
+	}
+	_putchar('\n');
+}
