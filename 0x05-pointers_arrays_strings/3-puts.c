0c0b5f5fd6cd7f07f313f59ec844bd13087a616b
ABOELDAHAB <karemali012345@gmail.com>
2023-08-21 18:11:08 -0700

    second task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints string
+ *
+ *@str: parameter that store string
+ *
+ * Return: Always 0 (success)
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
