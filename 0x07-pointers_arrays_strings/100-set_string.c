501513fe01d84cc43a50cea9c262bf990f22bff9
ABOELDAHAB <karemali012345@gmail.com>
2023-08-28 18:10:26 -0700

    second task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - library function
+ * @s: pointer of string
+ * @to: size of array
+ *
+ * Return: poistion
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
