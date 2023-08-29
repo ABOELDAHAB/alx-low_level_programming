970d6b3d5a7216af8bac41755706520dc60b2c69
ABOELDAHAB <karemali012345@gmail.com>
2023-08-28 18:04:02 -0700

    second task

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagsums - library function
+ * @a: pointer of string
+ * @size: size of array
+ *
+ * Return: poistion
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int c, k = 0, k2 = 0;
+
+	for (c = 0; c < size; c++)
+	{
+		k += a[c];
+		k2 += a[size - c - 1];
+		a += size;
+	}
+	printf("%d, ", k);
+	printf("%d\n", k2);
+}
