bb8c9de27cbfdb79bdb0d844f74938ecba688ed0
ABOELDAHAB <karemali012345@gmail.com>
2023-09-10 11:10:15 -0700

    second task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursion function
+ * @s: parameters
+ *
+ * Return : 0 (success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(*s + 1);
+	}
+}
