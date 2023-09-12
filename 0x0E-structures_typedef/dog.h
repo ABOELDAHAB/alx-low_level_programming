55e8bcfae49f10ed63740ae3d8bef463d74a1a48
ABOELDAHAB <karemali012345@gmail.com>
2023-09-12 14:22:06 -0700

    second task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure that has three members
+ * @name: member
+ * @age: member
+ * @owner: member
+ * Description: struct that has three members
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
