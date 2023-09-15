dd82b9cccded9947500d06ef9176877688d1e8f1
ABOELDAHAB <karemali012345@gmail.com>
2023-09-15 11:38:06 -0700

    second task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int s = 0, i = n;
-	va_list ap;
+	va_list k;
 
 	if (!n)
 		return (0);
-	va_start(ap, n);
+	va_start(k, n);
 	while (i--)
-		s += va_arg(ap, int);
-	va_end(ap);
+		s += va_arg(k, int);
+	va_end(k);
 	return (s);
 }
