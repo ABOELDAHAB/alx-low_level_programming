d3178da40021ef870cb2d019fb6edac05706f78d
ABOELDAHAB <karemali012345@gmail.com>
2023-09-26 11:18:34 -0700

    second task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - func
+ * @head: param
+ * Return: value
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *k;
+	int i;
+
+	if (!head || !*head)
+		return (0);
+
+	k = (*head)->next;
+	i = (*head)->i;
+	free(*head);
+	*head = k;
+	return (i);
+}
