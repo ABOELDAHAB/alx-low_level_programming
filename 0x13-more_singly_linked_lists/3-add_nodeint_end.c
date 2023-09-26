b773f0ced0e4049e991c8dbadb303729ca58fa7b
ABOELDAHAB <karemali012345@gmail.com>
2023-09-26 10:59:16 -0700

    second task

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - funct
+ * @head: param
+ * @n: value
+ * Return: sum
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *k = malloc(sizeof(listint_t));
+	listint_t *l;
+
+	if (!head || k)
+		return (NULL);
+
+	k->next = NULL;
+	k->n = n;
+	if (!*head)
+		*head = k;
+	else
+	{
+		l = *head;
+		while (l->next)
+			l = l->next;
+		l->next = k;
+	}
+	return (k);
+}
