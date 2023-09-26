5df975528d88bdcb705be47da12d379bcda32231
ABOELDAHAB <karemali012345@gmail.com>
2023-09-26 11:49:00 -0700

    second task

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_listint - fun
+ * @head: param
+ * Return : 0
+*/
+int sum_listint(listint_t *head)
+{
+	int result = 0;
+
+	while (head)
+	{
+		result += head->n;
+		head = head->next;
+	}
+	return (result);
+}
